561d995fcd67e586ac19cfa2ca1c85725b390635
jaso Sorenson <jaso@cs.wisc.edu>
2023-02-14 15:22:51 -0600

    more refactoring

--- Common/common.c
+++ Common/common.c
@@ -4,5 +4,5 @@
 
 
 void print_mac(uint8_t* mac) {
-    printf("%02" PRIx8 ":%02" PRIx8 ":%02" PRIx8 ":%02" PRIx8 ":%02" PRIx8 ":%02" PRIx8 "", mac[0], mac[1], mac[2], mac[3], mac[4] ,mac[5]);
+    printf("%02x:%02x:%02x:%02x:%02x:%02x", mac[0], mac[1], mac[2], mac[3], mac[4] ,mac[5]);
 }
