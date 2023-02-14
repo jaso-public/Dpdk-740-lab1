c4d896a5c1bf62397be4ad8d9d6a285014e99eb9
jaso <jaso@localhost>
2023-02-14 05:41:16 -0700

    minor code clean up

--- Client/lab1-client.c
+++ Client/lab1-client.c
@@ -26,6 +26,8 @@
 #define BURST_SIZE 32
 uint32_t NUM_PING = 100;
 
+#define BILLION (1000000000L)
+
 /* Define the mempool globally */
 struct rte_mempool *mbuf_pool = NULL;
 static struct rte_ether_addr my_eth;
@@ -40,43 +42,37 @@ int flow_num = 1;
 
 
 static uint64_t raw_time(void) {
-    struct timespec tstart={0,0};
+    struct timespec tstart;
     clock_gettime(CLOCK_MONOTONIC, &tstart);
-    uint64_t t = (uint64_t)(tstart.tv_sec*1.0e9 + tstart.tv_nsec);
-    return t;
-
+    return ((uint64_t)tstart.tv_sec)*BILLION + ((uint64_t)tstart.tv_nsec);
 }
 
 static uint64_t time_now(uint64_t offset) {
     return raw_time() - offset;
 }
 
-uint32_t
-checksum(unsigned char *buf, uint32_t nbytes, uint32_t sum)
-{
-	unsigned int	 i;
+uint32_t checksum(unsigned char *buf, uint32_t nbytes, uint32_t sum) {
+    unsigned int     i;
 
-	/* Checksum all the pairs of bytes first. */
-	for (i = 0; i < (nbytes & ~1U); i += 2) {
-		sum += (uint16_t)ntohs(*((uint16_t *)(buf + i)));
-		if (sum > 0xFFFF)
-			sum -= 0xFFFF;
-	}
+    /* Checksum all the pairs of bytes first. */
+    for (i = 0; i < (nbytes & ~1U); i += 2) {
+        sum += (uint16_t)ntohs(*((uint16_t *)(buf + i)));
+        if (sum > 0xFFFF)
+            sum -= 0xFFFF;
+    }
 
-	if (i < nbytes) {
-		sum += buf[i] << 8;
-		if (sum > 0xFFFF)
-			sum -= 0xFFFF;
-	}
+    if (i < nbytes) {
+        sum += buf[i] << 8;
+        if (sum > 0xFFFF)
+            sum -= 0xFFFF;
+    }
 
-	return sum;
+    return sum;
 }
 
-uint32_t
-wrapsum(uint32_t sum)
-{
-	sum = ~sum & 0xFFFF;
-	return htons(sum);
+uint32_t wrapsum(uint32_t sum) {
+    sum = ~sum & 0xFFFF;
+    return htons(sum);
 }
 
 static int parse_packet(struct sockaddr_in *src,
@@ -97,21 +93,26 @@ static int parse_packet(struct sockaddr_in *src,
     struct rte_ether_hdr * const eth_hdr = (struct rte_ether_hdr *)(p);
     p += sizeof(*eth_hdr);
     header += sizeof(*eth_hdr);
-    uint16_t eth_type = ntohs(eth_hdr->ether_type);
-    struct rte_ether_addr mac_addr = {};
 
+    // we should get the mac address once from dpdk, instead with each packet
+    struct rte_ether_addr mac_addr = {};
     rte_eth_macaddr_get(1, &mac_addr);
     if (!rte_is_same_ether_addr(&mac_addr, &eth_hdr->dst_addr)) {
-        printf("Bad MAC: %02" PRIx8 " %02" PRIx8 " %02" PRIx8
-			   " %02" PRIx8 " %02" PRIx8 " %02" PRIx8 "\n",
+        printf("Bad MAC: %02" PRIx8 " %02" PRIx8 " %02" PRIx8 " %02" PRIx8 " %02" PRIx8 " %02" PRIx8 
+	       " expected: %02" PRIx8 " %02" PRIx8 " %02" PRIx8 " %02" PRIx8 " %02" PRIx8 " %02" PRIx8 "\n",
             eth_hdr->dst_addr.addr_bytes[0], eth_hdr->dst_addr.addr_bytes[1],
-			eth_hdr->dst_addr.addr_bytes[2], eth_hdr->dst_addr.addr_bytes[3],
-			eth_hdr->dst_addr.addr_bytes[4], eth_hdr->dst_addr.addr_bytes[5]);
+            eth_hdr->dst_addr.addr_bytes[2], eth_hdr->dst_addr.addr_bytes[3],
+            eth_hdr->dst_addr.addr_bytes[4], eth_hdr->dst_addr.addr_bytes[5],
+            mac_addr.addr_bytes[0], mac_addr.addr_bytes[1],
+            mac_addr.addr_bytes[2], mac_addr.addr_bytes[3],
+            mac_addr.addr_bytes[4], mac_addr.addr_bytes[5]);
         return 1;
     }
-    if (RTE_ETHER_TYPE_IPV4 != eth_type) {
-        printf("Bad ether type\n");
-        return 0;
+
+    uint16_t eth_type = ntohs(eth_hdr->ether_type);
+    if (eth_type != RTE_ETHER_TYPE_IPV4) {
+        printf("Bad ether type:%d expected:%d\n", eth_type, RTE_ETHER_TYPE_IPV4);
+        return 1;
     }
 
     // check the IP header
@@ -140,29 +141,29 @@ static int parse_packet(struct sockaddr_in *src,
     in_port_t udp_src_port = udp_hdr->src_port;
     in_port_t udp_dst_port = udp_hdr->dst_port;
     int ret = 0;
-	
+    
 
-	uint16_t p1 = rte_cpu_to_be_16(5001);
-	uint16_t p2 = rte_cpu_to_be_16(5002);
-	uint16_t p3 = rte_cpu_to_be_16(5003);
-	uint16_t p4 = rte_cpu_to_be_16(5004);
-	
-	if (udp_hdr->dst_port ==  p1)
-	{
-		ret = 1;
-	}
-	if (udp_hdr->dst_port ==  p2)
-	{
-		ret = 2;
-	}
-	if (udp_hdr->dst_port ==  p3)
-	{
-		ret = 3;
-	}
-	if (udp_hdr->dst_port ==  p4)
-	{
-		ret = 4;
-	}
+    uint16_t p1 = rte_cpu_to_be_16(5001);
+    uint16_t p2 = rte_cpu_to_be_16(5002);
+    uint16_t p3 = rte_cpu_to_be_16(5003);
+    uint16_t p4 = rte_cpu_to_be_16(5004);
+    
+    if (udp_hdr->dst_port ==  p1)
+    {
+        ret = 1;
+    }
+    if (udp_hdr->dst_port ==  p2)
+    {
+        ret = 2;
+    }
+    if (udp_hdr->dst_port ==  p3)
+    {
+        ret = 3;
+    }
+    if (udp_hdr->dst_port ==  p4)
+    {
+        ret = 4;
+    }
 
     src->sin_port = udp_src_port;
     dst->sin_port = udp_dst_port;
@@ -186,90 +187,89 @@ static int parse_packet(struct sockaddr_in *src,
 static inline int
 port_init(uint16_t port, struct rte_mempool *mbuf_pool)
 {
-	struct rte_eth_conf port_conf;
-	const uint16_t rx_rings = 1, tx_rings = 1;
-	uint16_t nb_rxd = RX_RING_SIZE;
-	uint16_t nb_txd = TX_RING_SIZE;
-	int retval;
-	uint16_t q;
-	struct rte_eth_dev_info dev_info;
-	struct rte_eth_txconf txconf;
+    struct rte_eth_conf port_conf;
+    const uint16_t rx_rings = 1, tx_rings = 1;
+    uint16_t nb_rxd = RX_RING_SIZE;
+    uint16_t nb_txd = TX_RING_SIZE;
+    int retval;
+    uint16_t q;
+    struct rte_eth_dev_info dev_info;
+    struct rte_eth_txconf txconf;
 
-	if (!rte_eth_dev_is_valid_port(port))
-		return -1;
+    if (!rte_eth_dev_is_valid_port(port))
+        return -1;
 
-	memset(&port_conf, 0, sizeof(struct rte_eth_conf));
+    memset(&port_conf, 0, sizeof(struct rte_eth_conf));
 
-	retval = rte_eth_dev_info_get(port, &dev_info);
-	if (retval != 0)
-	{
-		printf("Error during getting device (port %u) info: %s\n",
-			   port, strerror(-retval));
-		return retval;
-	}
+    retval = rte_eth_dev_info_get(port, &dev_info);
+    if (retval != 0)
+    {
+        printf("Error during getting device (port %u) info: %s\n",
+               port, strerror(-retval));
+        return retval;
+    }
 
-	if (dev_info.tx_offload_capa & RTE_ETH_TX_OFFLOAD_MBUF_FAST_FREE)
-		port_conf.txmode.offloads |=
-			RTE_ETH_TX_OFFLOAD_MBUF_FAST_FREE;
+    if (dev_info.tx_offload_capa & RTE_ETH_TX_OFFLOAD_MBUF_FAST_FREE)
+        port_conf.txmode.offloads |=
+            RTE_ETH_TX_OFFLOAD_MBUF_FAST_FREE;
 
-	/* Configure the Ethernet device. */
-	retval = rte_eth_dev_configure(port, rx_rings, tx_rings, &port_conf);
-	if (retval != 0)
-		return retval;
+    /* Configure the Ethernet device. */
+    retval = rte_eth_dev_configure(port, rx_rings, tx_rings, &port_conf);
+    if (retval != 0)
+        return retval;
 
-	retval = rte_eth_dev_adjust_nb_rx_tx_desc(port, &nb_rxd, &nb_txd);
-	if (retval != 0)
-		return retval;
+    retval = rte_eth_dev_adjust_nb_rx_tx_desc(port, &nb_rxd, &nb_txd);
+    if (retval != 0)
+        return retval;
 
-	/* Allocate and set up 1 RX queue per Ethernet port. */
-	for (q = 0; q < rx_rings; q++)
-	{
-		retval = rte_eth_rx_queue_setup(port, q, nb_rxd,
-										rte_eth_dev_socket_id(port), NULL, mbuf_pool);
-		if (retval < 0)
-			return retval;
-	}
+    /* Allocate and set up 1 RX queue per Ethernet port. */
+    for (q = 0; q < rx_rings; q++)
+    {
+        retval = rte_eth_rx_queue_setup(port, q, nb_rxd,
+                                        rte_eth_dev_socket_id(port), NULL, mbuf_pool);
+        if (retval < 0)
+            return retval;
+    }
 
-	txconf = dev_info.default_txconf;
-	txconf.offloads = port_conf.txmode.offloads;
-	/* Allocate and set up 1 TX queue per Ethernet port. */
-	for (q = 0; q < tx_rings; q++)
-	{
-		retval = rte_eth_tx_queue_setup(port, q, nb_txd,
-										rte_eth_dev_socket_id(port), &txconf);
-		if (retval < 0)
-			return retval;
-	}
+    txconf = dev_info.default_txconf;
+    txconf.offloads = port_conf.txmode.offloads;
+    /* Allocate and set up 1 TX queue per Ethernet port. */
+    for (q = 0; q < tx_rings; q++)
+    {
+        retval = rte_eth_tx_queue_setup(port, q, nb_txd,
+                                        rte_eth_dev_socket_id(port), &txconf);
+        if (retval < 0)
+            return retval;
+    }
 
-	/* Starting Ethernet port. 8< */
-	retval = rte_eth_dev_start(port);
-	/* >8 End of starting of ethernet port. */
-	if (retval < 0)
-		return retval;
+    /* Starting Ethernet port. 8< */
+    retval = rte_eth_dev_start(port);
+    /* >8 End of starting of ethernet port. */
+    if (retval < 0)
+        return retval;
 
-	/* Display the port MAC address. */
-	retval = rte_eth_macaddr_get(port, &my_eth);
-	if (retval != 0)
-		return retval;
+    /* Display the port MAC address. */
+    retval = rte_eth_macaddr_get(port, &my_eth);
+    if (retval != 0)
+        return retval;
 
-	printf("Port %u MAC: %02" PRIx8 " %02" PRIx8 " %02" PRIx8
-		   " %02" PRIx8 " %02" PRIx8 " %02" PRIx8 "\n",
-		   port, RTE_ETHER_ADDR_BYTES(&my_eth));
+    printf("Port %u MAC: %02" PRIx8 " %02" PRIx8 " %02" PRIx8
+           " %02" PRIx8 " %02" PRIx8 " %02" PRIx8 "\n",
+           port, RTE_ETHER_ADDR_BYTES(&my_eth));
 
-	/* Enable RX in promiscuous mode for the Ethernet device. */
-	retval = rte_eth_promiscuous_enable(port);
-	/* End of setting RX port in promiscuous mode. */
-	if (retval != 0)
-		return retval;
+    /* Enable RX in promiscuous mode for the Ethernet device. */
+    retval = rte_eth_promiscuous_enable(port);
+    /* End of setting RX port in promiscuous mode. */
+    if (retval != 0)
+        return retval;
 
-	return 0;
+    return 0;
 }
 /* >8 End of main functional part of port initialization. */
 
 /* >8 End Basic forwarding application lcore. */
 
-static __rte_noreturn void
-lcore_main()
+static int lcore_main()
 {
     struct rte_mbuf *pkts[BURST_SIZE];
     struct rte_mbuf *pkt;
@@ -279,11 +279,12 @@ lcore_main()
     struct rte_udp_hdr *udp_hdr;
 
     // Specify the dst mac address here: 
-    struct rte_ether_addr dst = {{0x14,0x58,0xD0,0x58,0x5F,0x33}};
+    struct rte_ether_addr dst = {{0x14,0x58,0xD0,0x58,0xef,0xb3}};
 
-	struct sliding_hdr *sld_h_ack;
+    struct sliding_hdr *sld_h_ack;
     uint16_t nb_rx;
     uint64_t reqs = 0;
+    uint64_t intersend_time = 0;
     // uint64_t cycle_wait = intersend_time * rte_get_timer_hz() / (1e9);
     
     // TODO: add in scaffolding for timing/printing out quick statistics
@@ -324,11 +325,11 @@ lcore_main()
         ipv4_hdr->fragment_offset = 0;
         ipv4_hdr->time_to_live = 64;
         ipv4_hdr->next_proto_id = IPPROTO_UDP;
-        ipv4_hdr->src_addr = rte_cpu_to_be_32("127.0.0.1");
-        ipv4_hdr->dst_addr = rte_cpu_to_be_32("127.0.0.1");
+        ipv4_hdr->src_addr = rte_cpu_to_be_32(0x7f000001);
+        ipv4_hdr->dst_addr = rte_cpu_to_be_32(0x7f000001);
 
         uint32_t ipv4_checksum = wrapsum(checksum((unsigned char *)ipv4_hdr, sizeof(struct rte_ipv4_hdr), 0));
-        // printf("Checksum is %u\n", (unsigned)ipv4_checksum);
+        printf("Checksum is %u\n", (unsigned)ipv4_checksum);
         ipv4_hdr->hdr_checksum = rte_cpu_to_be_32(ipv4_checksum);
         header_size += sizeof(*ipv4_hdr);
         ptr += sizeof(*ipv4_hdr);
@@ -343,7 +344,7 @@ lcore_main()
 
         uint16_t udp_cksum =  rte_ipv4_udptcp_cksum(ipv4_hdr, (void *)udp_hdr);
 
-        // printf("Udp checksum is %u\n", (unsigned)udp_cksum);
+        printf("Udp checksum is %u\n", (unsigned)udp_cksum);
         udp_hdr->dgram_cksum = rte_cpu_to_be_16(udp_cksum);
         ptr += sizeof(*udp_hdr);
         header_size += sizeof(*udp_hdr);
@@ -369,7 +370,7 @@ lcore_main()
         }
         
         uint64_t last_sent = rte_get_timer_cycles();
-        // printf("Sent packet at %u, %d is outstanding, intersend is %u\n", (unsigned)last_sent, outstanding, (unsigned)intersend_time);
+        printf("Sent packet at %u, %d is outstanding, intersend is %u\n", (unsigned)last_sent, outstanding, (unsigned)intersend_time);
 
         /* now poll on receiving packets */
         nb_rx = 0;
@@ -400,7 +401,8 @@ lcore_main()
     }
     printf("Sent %"PRIu64" packets.\n", reqs);
     // dump_latencies(&latency_dist);
-    // return 0;
+    fprintf(stderr, "returning from lcore_main()\n");
+    return 0;
 }
 /*
  * The main function, which does initialization and calls the per-lcore
@@ -410,8 +412,8 @@ lcore_main()
 int main(int argc, char *argv[])
 {
 
-	unsigned nb_ports;
-	uint16_t portid;
+    unsigned nb_ports;
+    uint16_t portid;
 
     if (argc == 3) {
         flow_num = (int) atoi(argv[1]);
@@ -423,40 +425,38 @@ int main(int argc, char *argv[])
 
     NUM_PING = flow_size / packet_len;
 
-	/* Initializion the Environment Abstraction Layer (EAL). 8< */
-	int ret = rte_eal_init(argc, argv);
-	if (ret < 0)
-		rte_exit(EXIT_FAILURE, "Error with EAL initialization\n");
-	/* >8 End of initialization the Environment Abstraction Layer (EAL). */
+    /* Initializion the Environment Abstraction Layer (EAL). 8< */
+    int ret = rte_eal_init(argc, argv);
+    if (ret < 0) rte_exit(EXIT_FAILURE, "Error with EAL initialization\n");
 
-	argc -= ret;
-	argv += ret;
+    /* >8 End of initialization the Environment Abstraction Layer (EAL). */
+
+    argc -= ret;
+    argv += ret;
 
     nb_ports = rte_eth_dev_count_avail();
-	/* Allocates mempool to hold the mbufs. 8< */
-	mbuf_pool = rte_pktmbuf_pool_create("MBUF_POOL", NUM_MBUFS * nb_ports,
-										MBUF_CACHE_SIZE, 0, RTE_MBUF_DEFAULT_BUF_SIZE, rte_socket_id());
-	/* >8 End of allocating mempool to hold mbuf. */
+    /* Allocates mempool to hold the mbufs. 8< */
+    mbuf_pool = rte_pktmbuf_pool_create("MBUF_POOL", NUM_MBUFS * nb_ports, MBUF_CACHE_SIZE, 0, RTE_MBUF_DEFAULT_BUF_SIZE, rte_socket_id());
+    /* >8 End of allocating mempool to hold mbuf. */
 
-	if (mbuf_pool == NULL)
-		rte_exit(EXIT_FAILURE, "Cannot create mbuf pool\n");
+    if (mbuf_pool == NULL) rte_exit(EXIT_FAILURE, "Cannot create mbuf pool\n");
 
-	/* Initializing all ports. 8< */
-	RTE_ETH_FOREACH_DEV(portid)
-	if (portid == 1 && port_init(portid, mbuf_pool) != 0)
-		rte_exit(EXIT_FAILURE, "Cannot init port %" PRIu16 "\n",
-				 portid);
-	/* >8 End of initializing all ports. */
+    /* Initializing all ports. 8< */
+    RTE_ETH_FOREACH_DEV(portid)
+    if (portid == 1 && port_init(portid, mbuf_pool) != 0)
+        rte_exit(EXIT_FAILURE, "Cannot init port %" PRIu16 "\n", portid);
+    /* >8 End of initializing all ports. */
 
-	if (rte_lcore_count() > 1)
-		printf("\nWARNING: Too many lcores enabled. Only 1 used.\n");
+    if (rte_lcore_count() > 1)
+        printf("\nWARNING: Too many lcores enabled. Only 1 used.\n");
 
-	/* Call lcore_main on the main core only. Called on single lcore. 8< */
-	lcore_main();
-	/* >8 End of called on single lcore. */
-    printf("Done!\n");
-	/* clean up the EAL */
-	rte_eal_cleanup();
+    /* Call lcore_main on the main core only. Called on single lcore. 8< */
+    lcore_main();
+    /* >8 End of called on single lcore. */
 
-	return 0;
+    fprintf(stderr, "Done!\n");
+    /* clean up the EAL */
+    rte_eal_cleanup();
+
+    return 0;
 }
